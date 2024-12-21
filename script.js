8cf6e9436e1d2f32e63d8d8691348d1a961227fa
MASUR89 <surmava9@gmail.com>
2024-12-21 22:25:21 +0400

    უკუთვლა

---
+++ script.js
@@ -0,0 +1,19 @@
+// უკუთვლას ვაკეთებ ახალ წლამდე
+const newYear = new Date('January 1, 2025 00:00:00');
+const interval = setInterval(calculateTime, 1000);
+
+function calculateTime() {
+    const now = new Date();
+    const remainingTime = newYear - now; 
+    
+    // დარჩენილ; დრო გადამყავს დღეებში, საათებში, წუთებში და წამებში
+    const days = Math.floor(remainingTime / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((remainingTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((remainingTime % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((remainingTime % (1000 * 60)) / 1000);
+    
+    document.getElementById('days').textContent = days.toString().padStart(2, '0');
+    document.getElementById('hours').textContent = hours.toString().padStart(2, '0');
+    document.getElementById('minutes').textContent = minutes.toString().padStart(2, '0');
+    document.getElementById('seconds').textContent = seconds.toString().padStart(2, '0');
+}
